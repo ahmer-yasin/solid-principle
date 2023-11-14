9cdee57f4600fa10c71d3f6785add85d82fc7a0d
Ahmer Yasin <ahmeryasin1992@gmail.com>
2023-11-14 12:01:59 +0400

    solid principle implementation

---
+++ src/book.ts
@@ -0,0 +1,16 @@
+class Book {
+    name: string;
+    authorName: string;
+    year: number;
+    price: number;
+  
+    constructor(name: string, authorName: string, year: number, price: number) {
+      this.name = name;
+      this.authorName = authorName;
+      this.year = year;
+      this.price = price;
+    }
+  }
+  
+  export default Book;
+  
